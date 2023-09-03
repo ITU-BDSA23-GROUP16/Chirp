11a1153f846de88839e517488f84e1aff746bfa5
Stanley Lee <54396857+SWLC0099@users.noreply.github.com>
2023-09-03 16:53:12 +0200

    Added comments

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -12,20 +12,27 @@ class Program
             // Open the text file using a stream reader.
             using (var sr = new StreamReader(file))
             {
+                //Defines variables for later use
                 string line;
                 string author;
                 string message;
+                //First command line argument "read" or cheep"
                 string command = args[0];
-
+                
+                //Displays cheeps
                 if(command == "read")
                 {
+                    //loops through each line of csv file
                     while ((line = sr.ReadLine()) != null)
                     {
                     //Define pattern
                     Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
                     //Separating columns to array
                     string[] X = CSVParser.Split(line);
 
+                    //check if there are three columns author, message, timestamp in each line
+                    //converts unixtime to readable date and prints formatted line to console
                     if(X.Length >= 3) 
                     {
                         author = X[0];
@@ -40,8 +47,13 @@ class Program
                         
                     } 
                     }
+                //adds cheep
                 } else if(command == "cheep")
                 {
+                    //checks if there are at least two command line arguments besides cheep itself
+                    //if there are at least 2 arguments
+                    //sets author to logged in user's name
+                    //create new message with author, message and unix timestamp
                     if(args.Length >= 2)
                     {
                     author = Environment.UserName;
@@ -49,14 +61,15 @@ class Program
                     long unixTimeStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
                     string date = DateTimeOffset.UtcNow.ToString("MM/dd/yy HH:mm:ss");
                     string newLine = $"{author},\"{message}\",{unixTimeStamp}";
+                    //appends new line to csv file using StreamWriter
                     using (StreamWriter sw = File.AppendText(file))
                     {
-                        // Append the new line to the file
                         sw.WriteLine(newLine);
                     }
                     }
                 }
                 // Read the stream as a string, and write the string to the console.
+                //i.e. prints contents of csv file to console
                 Console.WriteLine(sr.ReadToEnd());
             }
         }
