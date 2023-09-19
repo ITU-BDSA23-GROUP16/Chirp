52d6ba75e75f02c2128fa726f60018e9b5259a67
christina <chdi@itu.dk>
2023-09-19 11:19:05 +0200

    Refactor UserInterface class #16
    To ensure single responsibility I refactored the class methods

--- src/Chirp.CLI/UserInterface.cs
+++ src/Chirp.CLI/UserInterface.cs
@@ -1,11 +1,21 @@
-static class UserInterface {
+namespace UI;
 
-public static void PrintCheeps(IEnumerable<Cheep> cheeps){
-     foreach (Cheep cheep in cheeps){
-        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp);
+public static class UserInterface {
+
+    public static string ConvertTimestampToDate(long timestamp){
+        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(timestamp);
         string date = dateTimeOffset.ToString("MM/dd/yy HH:mm:ss");
-        Console.WriteLine($"{cheep.Author} @ {date}: {cheep.Message}");
+        return date;
+    }
+
+    public static string GetCheepFormattedMessage(Cheep cheep){
+        string date = ConvertTimestampToDate(cheep.Timestamp);
+        return $"{cheep.Author} @ {date}: {cheep.Message}";
+    }
+
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps){
+        foreach (Cheep cheep in cheeps){
+            Console.WriteLine(GetCheepFormattedMessage(cheep));
         }
     }
-   
 }
