b4ce65483e542e30c81bf6138508632fb26f70c3
dima <dimj@itu.dk>
2023-10-03 12:46:06 +0200

    Added errorhandling (ArgumentException)
    Added errorhandling so that the website won't crash if the size of the returned cheeps is invalid
    
    Co-authored-by: Stanley Christensen <stwc@itu.dk>

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -14,24 +14,38 @@ public class PublicModel : PageModel
     }
 
     public ActionResult OnGet()
-
     {
-        int PageInt;
-        if (string.IsNullOrEmpty(PageNumber))
+        try
         {
-            PageInt = 1;
-        }
-        else
-        {
-            PageInt = Int32.Parse(PageNumber);
-        }
-        Cheeps = _service.GetCheeps();
+            int PageInt;
+            if (string.IsNullOrEmpty(PageNumber))
+            {
+                PageInt = 1;
+            }
+            else
+            {
+                PageInt = Int32.Parse(PageNumber);
+            }
+            Cheeps = _service.GetCheeps();
 
-        Cheeps = Cheeps.GetRange(((PageInt - 1) * 5), 5);
-        return Page();
+
+            Cheeps = Cheeps.GetRange(((PageInt - 1) * 32), 32);
+            
+        }
+        catch (ArgumentException e)
+        {
+            if (Cheeps.Count > _service.GetCheeps().Count)
+            {
+                throw new ArgumentException("Argument is invalid");
+            }
+        } 
+       return Page();
+        }    
+
+
+     [BindProperty(SupportsGet = true)]
+    public string? PageNumber { get; set; }
+       
     }
 
-    [BindProperty(SupportsGet = true)]
-    public string? PageNumber { get; set; }
-
-}
+ 
