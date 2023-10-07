021a62e43d3ca28109e21ffce1265ac6dbb5dca4
Saynab <saym@itu.dk>
2023-10-07 17:06:53 +0200

    New migration and modified program.cs
    We have created a new migration, that fits the changes made to the Cheep and Author entities. The program.cs file has been updated to ensure it functions with the Cheep and Author entities
    
    Co-authored-by: Dima Jafar <dimj@itu.dk>

--- src/Chirp.Core/Model.cs
+++ src/Chirp.Core/Model.cs
@@ -2,34 +2,45 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 
-public class BloggingContext : DbContext
+
+//dotnet tool install --global dotnet-ef
+
+public class ChirpContext : DbContext
 {
-    public DbSet<Author> author { get; set; }
-    public DbSet<Cheep> cheep { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Cheep> Cheeps { get; set; }
+
+     
 
     public string DbPath { get; }
 
-    public BloggingContext()
+    public ChirpContext()
     {
+        
         var folder = Environment.SpecialFolder.LocalApplicationData;
         var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "blogging.db");
+         DbPath = System.IO.Path.Join(path, "chirp.db");
     }
 
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
+    //dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 7.0.11 (for using the UseSqlite)
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");
 }
 
 
+
+//https://learn.microsoft.com/en-us/ef/core/modeling/keyless-entity-types?tabs=data-annotations
+// A problem is that Cheep requires a primary key in the old code Post() had a PostId
 public class Cheep
 {
+    public int CheepId { get; set; } 
     public string Text { get; set; }
 
     public DateTime Time { get; set; }
 
-    public Author? Author {get; set}
+    public Author? Author {get; set;} 
 
 
 
@@ -37,13 +48,18 @@ public class Cheep
 
 //https://learn.microsoft.com/en-us/ef/core/modeling/relationships/one-to-many
 //The link above is a Microsoft documentation about one-to-many relationships in EF Core that has been used in line 45
+
+//https://learn.microsoft.com/en-us/ef/core/modeling/keyless-entity-types?tabs=data-annotations
+// A problem is that Author requires a primary key in the old code Blog() had a BlogID
+
 public class Author
 {
+    public int AuthorId { get; set; } 
     public string Email { get; set; }
 
     public string Name { get; set; }
 
-     public ICollection<Cheep> cheep {get; set} = new List<Cheep>()
+     public ICollection<Cheep> cheep {get; set;} = new List<Cheep>();
  
 
 }
--- src/Chirp.Core/Program.cs
+++ src/Chirp.Core/Program.cs
@@ -1,6 +1,49 @@
 ﻿using System;
 using System.Linq;
 
+//This line creates a new instance of the ChirpContext, which is a database context class for Entity Framework Core. 
+//It establishes a connection to the database specified in your context's configuration
+using var db = new ChirpContext();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new author");
+//Creates a new author object with an Email and a Name
+var author = new Author { Email = "Say123@example.com", Name = "Saynab Liibaan" };
+//Adds to the Authors DbSet (database table) within the ChirpContext.
+db.Add(author);
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for an author");
+//Creating a new author by retreiving an author from the database by using db.Authors
+//it sorts all emails in ascending order: https://www.tutorialsteacher.com/linq/linq-sorting-operators-orderby-orderbydescending#google_vignette
+var retrievedAuthor = db.Authors
+    .OrderBy(a => a.Email)
+    .First();
+
+
+// Update
+Console.WriteLine("Updating the author and adding a cheep");
+//This part updates the Name value of the retrievedAuthor from "Saynab Liibaan" to "Saynab"
+retrievedAuthor.Name = "Saynab";
+//This code of line adds a new cheep assosicated with the author by using the cheep collection
+retrievedAuthor.cheep.Add(new Cheep { Text = "Whats uuuuuuup!", Time = DateTime.Now });
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the author");
+//And u can delete an author from the database by using the Remove()
+db.Remove(retrievedAuthor);
+db.SaveChanges();
+
+
+
+/*
+using System.Linq;
+
 using var db = new BloggingContext();
 
 // Note: This sample requires the database to be created before running.
@@ -28,3 +71,7 @@ db.SaveChanges();
 Console.WriteLine("Delete the blog");
 db.Remove(blog);
 db.SaveChanges();
+
+
+
+*/
