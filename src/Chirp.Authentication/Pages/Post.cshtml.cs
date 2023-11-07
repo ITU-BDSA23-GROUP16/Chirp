9b0f82cdfcb559dad89e5c80a88b66a691dd32cd
Stanley Lee <stwc@itu.dk>
2023-11-07 13:08:26 +0100

    Setup submission form to print Cheep posting
    Co-authored-by: Jeppe Balslev <jebal@itu.dk>

---
+++ src/Chirp.Authentication/Pages/Post.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Authentication.Pages;
+
+public class PostModel : PageModel
+{
+    public String Text { get; set; }
+    private readonly ILogger<PostModel> _logger;
+
+    public PostModel(ILogger<PostModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+    public void OnPost(string message)
+    {
+        Console.WriteLine($"{message}");
+    }
+}
+
