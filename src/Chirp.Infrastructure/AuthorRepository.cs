2b88afa8b5ce32d9fe15a61ce41cc95ade5b8dfe
Stanley Lee <stwc@itu.dk>
2023-11-14 16:44:09 +0100

    Tests and webapp works
    Revert changes so that 4/8 tests work albeit with a different error
    
    Webapp also works and has been tested manually

--- src/Chirp.Infrastructure/AuthorRepository.cs
+++ src/Chirp.Infrastructure/AuthorRepository.cs
@@ -30,7 +30,7 @@ public class AuthorRepository : IAuthorRepository
         await _context.SaveChangesAsync();
     }
 
-   /* private static ICollection<CheepDTO> ConvertCheeps(ICollection<Cheep> cheeps) {
+    /* private static ICollection<CheepDTO> ConvertCheeps(ICollection<Cheep> cheeps) {
         var dtoColl = new List<CheepDTO>();
         foreach (Cheep c in cheeps) {
             var dtoConv = new CheepDTO(c.Author.Name,c.Message,c.TimeStamp);
@@ -42,18 +42,18 @@ public class AuthorRepository : IAuthorRepository
 
     public async Task<AuthorDTO> FindAuthorByName(string author){
     
-      return await _context.Authors
-      .Where(a => a.UserName.Contains(author))
-      .OrderByDescending(a => a.UserName)
-      .Select(a => new AuthorDTO(a!.UserName, a.Email, a.Cheeps.Select(c => new CheepDTO(c.Author.UserName,c.Message,c.TimeStamp)))).FirstOrDefaultAsync();;
+    return await _context.Authors
+    .Where(a => a.UserName.Contains(author))
+    .OrderByDescending(a => a.UserName)
+    .Select(a => new AuthorDTO(a!.UserName, a.Email, a.Cheeps.Select(c => new CheepDTO(c.Author.UserName,c.Message,c.TimeStamp)))).SingleOrDefaultAsync();
     } 
 
     
     public async Task<AuthorDTO> FindAuthorByEmail(string email){
     return await _context.Authors 
-      .Where(a => a.UserName.Contains(email))
-      .OrderByDescending(a => a.Email)
-      .Select(a => new AuthorDTO(a!.UserName, a.Email, a.Cheeps.Select(c => new CheepDTO(c.Author.UserName,c.Message,c.TimeStamp)))).FirstOrDefaultAsync();;
+    .Where(a => a.Email!=null && a.Email==email)
+    .OrderByDescending(a => a.Email)
+    .Select(a => new AuthorDTO(a!.UserName, a.Email, a.Cheeps.Select(c => new CheepDTO(c.Author.UserName,c.Message,c.TimeStamp)))).SingleOrDefaultAsync();
         
 
     }
