c3665fc25a055764ba52947a156caa56263cbe42
christina <chdi@itu.dk>
2023-12-05 15:37:06 +0100

    Forget me button
    Changed delete button to fit requirements and also log out once deleted.

--- src/Chirp.Web/Pages/MyPage.cshtml.cs
+++ src/Chirp.Web/Pages/MyPage.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using Chirp.Core;
+using Microsoft.AspNetCore.Identity;
 namespace Chirp.Web.Pages;
 
 public class MyPageModel : PageModel
@@ -9,20 +10,24 @@ public class MyPageModel : PageModel
 
     protected readonly ILogger<TimelineModel> _logger;
     protected readonly IAuthorRepository _repository;
+    private readonly SignInManager<Author> _signInManager;
 
 
 
-    public MyPageModel(ILogger<TimelineModel> logger, IAuthorRepository repository)
+
+    public MyPageModel(ILogger<TimelineModel> logger, IAuthorRepository repository, SignInManager<Author> signInManager)
     {
         _logger = logger;
         _repository = repository;
+        _signInManager = signInManager;
+
     }
     public void OnGet()
     {
     }
 
 
-    public async Task OnPostDeleteAsync(string author)
+    public async Task<IActionResult> OnPostDeleteAsync(string author)
     {
         if (author == null)
         {
@@ -31,6 +36,9 @@ public class MyPageModel : PageModel
         else
         {
             await _repository.DeleteAuthor(author);
+            await _signInManager.SignOutAsync();
+
         }
+        return Redirect("/");
     }
 }
